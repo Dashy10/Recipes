12927bac374da481a241182bd33b4aa1979890bc
Dashy10 <dashiell.lumas@gmail.com>
2017-05-12 12:18:53 -0400

    initialize

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import JSONData  from '../../json/recipe'
+import './App.css';
+console.log('JSONData', JSONData)
+class App extends React.Component {
+  render() {
+    return (
+      <div>I AM APP!!!!!!!!!!!!!!! {JSONData.foo}</div>
+    )
+  }
+}
+
+export default App;
